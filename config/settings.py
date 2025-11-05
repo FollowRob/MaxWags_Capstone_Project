6a2df7a2bfa0db07cc474a1428cda82df5eb46c2
Rob Smith <rsmith0393@gmail.com>
2025-11-05 11:44:26 +0000

    setdebug to false

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ if os.path.isfile('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['capstone-maxwags-e4277e29559a.herokuapp.com', 'localhost', '127.0.0.1']
 
